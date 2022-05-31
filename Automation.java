73fbc9741e3620483d81ee01b1e5193f68fa7f11
Malathi92 <103888118+Malathi92@users.noreply.github.com>
2022-05-31 12:13:12 +0400

    Add files via upload

---
+++ Automation.java
@@ -0,0 +1,35 @@
+package Week3.day2;
+
+public class Automation extends MultipleLanguage  {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Automation a=new Automation();
+		System.out.println("on abstraction");
+		a.java();
+		a.selenium();
+		a.ruby();
+		a.python();
+		}
+
+	public void java() {
+		// TODO Auto-generated method stub
+		System.out.println("java language");
+		
+	}
+
+	public void selenium() {
+		// TODO Auto-generated method stub
+		System.out.println("selenium");
+		
+	}
+
+	@Override
+	public void ruby() {
+		// TODO Auto-generated method stub
+		System.out.println("ruby");
+		
+	}
+
+}
---
+++ MultipleLanguage.java
@@ -0,0 +1,11 @@
+package Week3.day2;
+
+abstract class MultipleLanguage implements Language,TestTool {
+	public void python()
+	{
+		System.out.println("python");
+	}
+
+	public abstract void ruby();
+	
+}
